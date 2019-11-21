e17a791053b1bf4a6ef7aa4b8de1c6a3df0cb4c8
Fahd Magdy <Fahd.Magdy@itworx.com>
2019-11-21 15:12:22 +0200

    boo

---
+++ fourthproject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace fourthproject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
