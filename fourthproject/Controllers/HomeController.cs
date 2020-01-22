99fed592cd3c605708c699632dcbdb2c3b8f94a8
Fahd Magdy <Fahd.Magdy@itworx.com>
2020-01-22 14:53:46 +0200

    sql image

--- fourthproject/Controllers/HomeController.cs
+++ fourthproject/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.Mvc;
 
 namespace fourthproject.Controllers
@@ -15,6 +14,42 @@ namespace fourthproject.Controllers
 
         public ActionResult About()
         {
+            string connectionString =
+            "Data Source=172.20.192.101;Initial Catalog=DockerTesting;"
+            + "Integrated Security=false;User Id=sa;password=Xyz78901";
+
+            string queryString =
+            "INSERT INTO dbo.FirstTable ([Count]) VALUES ((select MAX([Count]) from dbo.FirstTable) +1)";
+
+
+            using (SqlConnection connection =
+            new SqlConnection(connectionString))
+            {
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+                // Open the connection in a try/catch block. 
+                // Create and execute the DataReader, writing the result
+                // set to the console window.
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}",
+                            reader[0], reader[1], reader[2]);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+
+
+
             ViewBag.Message = "Your application description page.";
 
             return View();
---
+++ fourthproject/SQL/Table - Copy.sql
@@ -0,0 +1,73 @@
+USE [master]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'DockerTesting')
+ BEGIN
+CREATE DATABASE [DockerTesting]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'DockerTesting', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\DockerTesting.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'DockerTesting_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\DockerTesting_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+END
+GO
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'DockerTesting')
+ BEGIN
+ALTER DATABASE [DockerTesting] SET COMPATIBILITY_LEVEL = 140
+END
+GO
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'DockerTesting')
+ BEGIN
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [DockerTesting].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+END
+GO
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'DockerTesting')
+ BEGIN
+ALTER DATABASE [DockerTesting] SET AUTO_SHRINK OFF 
+END
+GO
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'DockerTesting')
+ BEGIN
+ALTER DATABASE [DockerTesting] SET  MULTI_USER 
+END
+GO
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'DockerTesting')
+ BEGIN
+ALTER DATABASE [DockerTesting] SET  READ_WRITE 
+END
+GO
+
+use [DockerTesting]
+IF (NOT EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'FirstTable'))
+BEGIN
+CREATE TABLE [dbo].[FirstTable](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Count] [int] NULL,
+ CONSTRAINT [PK_FirstTable] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+ 
+ INSERT INTO dbo.FirstTable ([Count]) VALUES (1)
+END
+GO
+
+IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'FirstTable'))
+BEGIN
+ INSERT INTO dbo.FirstTable ([Count]) VALUES ((select MAX([Count]) from dbo.FirstTable) +1)
+END
